8def5a0d2eff8201503683d2542e354e4f63a6df
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-12-17 14:45:12 +0545

    feat:setting up workflows

--- src/views/authentication/Register/Register.tsx
+++ src/views/authentication/Register/Register.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
+      This is the register page
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Passwords" />
       <input type="password" placeholder="ConfirmPasswords" />
