3fc6c231f11f43052184742aeb89e9f6899e8a68
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-12-17 12:13:00 +0545

    feat:wip login and register

---
+++ src/views/authentication/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+      <input type="text" placeholder='Username' />
+      <input type="password" placeholder='Passwords' />
+    </div>
+  )
+}
+
+export default Login
---
+++ src/views/authentication/Register/Register.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Passwords" />
+      <input type="password" placeholder="ConfirmPasswords" />
+    </div>
+  );
+}
+
+export default Register
