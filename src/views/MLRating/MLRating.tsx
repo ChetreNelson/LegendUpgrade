230e948d6075e03aeab933a657b5f004f2a799c7
ChetreNelson <128913141+ChetreNelson@users.noreply.github.com>
2024-12-17 16:55:43 +0545

    feat:wip rating

--- src/views/MLRating/MLRating.tsx
+++ src/views/MLRating/MLRating.tsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const MLRating = () => {
-  return <div>work in progress rating page</div>;
+  return (
+    <div className="bg-primary w-full h-[100vh] ">
+      <div>header</div>
+      <div className="flex ">
+        <div className="">sidebar</div>
+        <div className="grid grid-cols-2 flex-1">
+          <div>div1</div>
+          <div>div2</div>
+        </div>
+      </div>
+      <div>footer</div>
+    </div>
+  );
 };
 
 export default MLRating;
